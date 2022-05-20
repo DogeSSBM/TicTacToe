69977bcdb4d2f4e7cb5f4abe3d8adc92e9bc2b21
DogeSSBM <dogessbm@gmail.com>
2022-05-19 23:03:09 -0400

    asdf

--- main.c
+++ main.c
@@ -2,23 +2,6 @@
 
 int main(int argc, char const *argv[])
 {
-	Length window = {800, 600};
-	init();
-	setWindowLen(window);
-
-	while(1){
-        Ticks t = frameStart();
-
-        if(keyPressed(SDL_SCANCODE_SPACE)){
-            printf("Space pressed\n");
-            drawTextCenteredCoord("Space pressed", getWindowMid());
-        }
-        if(mouseBtnState(MOUSE_L)){
-            if(mouseBtnPressed(MOUSE_L))
-                printf("Left clicked\n");
-            drawTextCenteredCoord("Left clicked", mouse.pos);
-        }
-        frameEnd(t);
-	}
-	return 0;
+    
+    return 0;
 }
