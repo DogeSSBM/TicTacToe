3f079ff69196529c257ce7017515ab991a992a91
DogeSSBM <dogessbm@gmail.com>
2022-05-19 23:05:40 -0400

    asdf

---
+++ Includes.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>
+
+#endif /* end of include guard: INCLUDES_H */
