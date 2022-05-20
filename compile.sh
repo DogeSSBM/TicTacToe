e794d931a45144b52d30532d51a70d41540f736a
DogeSSBM <dogessbm@gmail.com>
2022-05-19 23:06:19 -0400

    asdf

--- compile.sh
+++ compile.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 printf "\033c"
-libraries="-lSDL2 -lSDL2_ttf -lSDL2_gfx -lSDL2_image -lSDL2_net -lSDL2_mixer"
-flags="-Wall -Wno-missing-braces -lm -O3 -Winline"
-gcc main.c $libraries $flags -o main.out
+gcc main.c -o main.out
